52e1f59ae4a989356d81dab3ac81df7373eb34e7
kekc85 <kekc85@yandex.ru>
2026-08-25 15:30:36 +0300

    конфигурация: интеграция реквизитов базы данных MySQL Beget (kekc8584_plan)

--- db_config.py
+++ db_config.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Конфигурация подключения к базе данных MySQL (Beget).
-Считывает параметры из переменных окружения или локального файла .env.
+Считывает параметры из переменных окружения или использует настройки базы данных Beget.
 """
 
 import os
@@ -10,9 +10,9 @@ import os
 # Параметры подключения к MySQL на хостинге Beget
 DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_PORT = int(os.environ.get("DB_PORT", "3306"))
-DB_USER = os.environ.get("DB_USER", "")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
-DB_NAME = os.environ.get("DB_NAME", "")
+DB_USER = os.environ.get("DB_USER", "kekc8584_plan")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "Y4vZI5p*0dmQ")
+DB_NAME = os.environ.get("DB_NAME", "kekc8584_plan")
 DB_CHARSET = "utf8mb4"
 
 # Секретный ключ для JWT токенов
